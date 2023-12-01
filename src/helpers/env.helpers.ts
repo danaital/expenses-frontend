2ca71d4aa28984e9438db1b4a5f001f5c309730c
taldanai1995 <taldanai@icloud.com>
2023-12-01 17:50:11 +0200

    eslint,prettier, husky, add envHelpers

---
+++ src/helpers/env.helpers.ts
@@ -0,0 +1,3 @@
+export const isDev = (): boolean => process.env.NODE_ENV === 'development';
+export const getBackendBase = ():string | undefined => process.env.REACT_APP_BACKEND_URL;
+export const getFrontendBase = ():string | undefined => process.env.REACT_APP_FRONTEND_URL;
