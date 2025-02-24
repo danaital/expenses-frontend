f41c0b3adbfc2348c2193f03d6779045522b3c50
Tal Danai <70094025+danaital@users.noreply.github.com>
2025-02-24 09:41:36 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { RouterApp } from './controls/routers/router-app';
-
+// TODO: add .gitignore
 function App() {
   return (
     <Router>
